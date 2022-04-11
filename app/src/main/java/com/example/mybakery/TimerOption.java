ffb63be65423e260d30d643350961fd6e184132e
Trident12kilo <dhdlsrnr1231@naver.com>
2022-04-11 19:47:48 +0900

    MyBakery / 알람기능 추가중 _ ListView 기능 수정중/

--- app/src/main/java/com/example/mybakery/TimerOption.java
+++ app/src/main/java/com/example/mybakery/TimerOption.java
@@ -36,6 +36,8 @@ public class TimerOption extends AppCompatActivity {
     SharedPreferences sp;
     SharedPreferences.Editor editor;
 
+    ArrayAdapter<Integer> SD_Adapter;
+
     ArrayList<Integer> SD_Index = new ArrayList<>(8);
     ArrayList<String> Schedule_Index = new ArrayList<>(8);
     ArrayList<Integer> Time_Index = new ArrayList<>(8);
@@ -49,29 +51,25 @@ public class TimerOption extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.option_timer_setting);
 
-        ArrayAdapter<Integer> SD_Adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,SD_Index);
+        SD_Adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,SD_Index);
 
         listView = findViewById(R.id.TimerList);
-
         listView.setAdapter(SD_Adapter);
 
         sp = getSharedPreferences("shared", MODE_PRIVATE);
         editor = sp.edit();
 
         add_Timer_Layout = (LinearLayout) findViewById(R.id.add_Timer_Layout);
-
         SavedData_Length = sp.getInt("SavedData_Length",0);
         
         if(SavedData_Length == 0){
             Toast.makeText(getApplicationContext(), "타이머를 생성 해주세요", Toast.LENGTH_SHORT).show();
-        } else {
-            for(int i = 0; i <= SavedData_Length; i++) {
-                String schedule = sp.getString("schedule", "텍스트 표기" + SavedData_Length);
-                int ScheduleCount = sp.getInt("time", 0);
-                TextView tvData = (TextView) findViewById(R.id.tv_data);
-                tvData.setText(SavedData_Length + schedule + ScheduleCount);
-            }
-        }
+        } /*else {
+            String schedule = sp.getString("schedule", "텍스트 표기" + SavedData_Length);
+            int ScheduleCount = sp.getInt("time", 0);
+            TextView tvData = (TextView) findViewById(R.id.tv_data);
+            tvData.setText(SavedData_Length + schedule + ScheduleCount);
+        }*/
         listView.setAdapter(SD_Adapter);
 
     }
@@ -93,37 +91,38 @@ public class TimerOption extends AppCompatActivity {
 
             listView.setAdapter(SD_Adapter);
 
-
-            for (int i = 0; i <= SavedData_Length; i++) {
-
-                editor.putInt("SavedData_Length", SavedData_Length);
-                editor.putString("schedule", "텍스트 표기" + SavedData_Length);
-                editor.putInt("time", SavedData_Length);
-                editor.commit(); //완료한다.
+            editor.putInt("SavedData_Length", SavedData_Length);
+            editor.putString("schedule", "텍스트 표기" + SavedData_Length);
+            editor.putInt("time", SavedData_Length);
+            editor.commit(); //완료한다.
 
 
-                SavedData_Length = sp.getInt("SavedData_Length", SavedData_Length);
-                schedule = sp.getString("schedule", "");
-                time = sp.getInt("time", 0);
+            SavedData_Length = sp.getInt("SavedData_Length", SavedData_Length);
+            schedule = sp.getString("schedule", "");
+            time = sp.getInt("time", 0);
 
-                SD_Index.add(SavedData_Length);
-                Schedule_Index.add(schedule);
-                Time_Index.add(time);
+            SD_Index.add(SavedData_Length);
+            Schedule_Index.add(schedule);
+            Time_Index.add(time);
 
-                TextView tvData = (TextView) findViewById(R.id.tv_data);
+            TextView tvData = (TextView) findViewById(R.id.tv_data);
 
-                tvData.setText(SavedData_Length + schedule + time);
-            }
+            tvData.setText(SavedData_Length + schedule + time);
+
         }
-
-
     }
 
 
     public void onDeleteData(View v) {
         sp = getSharedPreferences("shared", MODE_PRIVATE);
         editor = sp.edit();
+        SD_Adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,SD_Index);
         SavedData_Length = 0;
+        int AdCount = SD_Adapter.getCount();
+        for(int i = 0 ; i < AdCount; i++){
+            SD_Adapter.remove(i);
+        }
+        listView.setAdapter(SD_Adapter);
         editor.clear();
         editor.commit();
 
