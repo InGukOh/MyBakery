4e3e41e2aa1abf3c4fd8daad89a36fde09caabc7
Trident12kilo <dhdlsrnr1231@naver.com>
2022-04-12 00:02:13 +0900

    MyBakery / 알람기능 추가중 _ ListView 기능 수정중/ 2

--- app/src/main/java/com/example/mybakery/TimerOption.java
+++ app/src/main/java/com/example/mybakery/TimerOption.java
@@ -2,134 +2,130 @@ package com.example.mybakery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-
-import android.view.Gravity;
+import android.preference.PreferenceManager;
 import android.view.View;
-
-import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import java.lang.reflect.Array;
+import org.json.JSONArray;
+import org.json.JSONException;
+
 import java.util.ArrayList;
+import java.util.logging.Logger;
+
 
 public class TimerOption extends AppCompatActivity {
+
     int SavedData_Length; //저장된 데이터 갯수
     int count; // 타이머 갯수
     String schedule; // 일정 내용
     int time; // 일정의 시간
 
-    LinearLayout add_Timer_Layout;
     ListView listView;
-    EditText make_Schedule;
-    EditText make_Time;
-    Button make_dellBtn;
+    Button btn_add;
+    Button delAll;
 
     SharedPreferences sp;
     SharedPreferences.Editor editor;
 
     ArrayAdapter<Integer> SD_Adapter;
 
-    ArrayList<Integer> SD_Index = new ArrayList<>(8);
-    ArrayList<String> Schedule_Index = new ArrayList<>(8);
-    ArrayList<Integer> Time_Index = new ArrayList<>(8);
+    ArrayList<Integer> SD_Index = new ArrayList<>();
+    ArrayList<String> array = new ArrayList<>();
 
-    public int saveDATA(int count){
-        count = sp.getInt("count",SavedData_Length);;
-        return count;
-    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.option_timer_setting);
 
-        SD_Adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,SD_Index);
-
         listView = findViewById(R.id.TimerList);
         listView.setAdapter(SD_Adapter);
 
-        sp = getSharedPreferences("shared", MODE_PRIVATE);
-        editor = sp.edit();
+        array = getStringArrayList(getApplicationContext(),"test");
 
-        add_Timer_Layout = (LinearLayout) findViewById(R.id.add_Timer_Layout);
-        SavedData_Length = sp.getInt("SavedData_Length",0);
-        
-        if(SavedData_Length == 0){
-            Toast.makeText(getApplicationContext(), "타이머를 생성 해주세요", Toast.LENGTH_SHORT).show();
-        } /*else {
-            String schedule = sp.getString("schedule", "텍스트 표기" + SavedData_Length);
-            int ScheduleCount = sp.getInt("time", 0);
-            TextView tvData = (TextView) findViewById(R.id.tv_data);
-            tvData.setText(SavedData_Length + schedule + ScheduleCount);
-        }*/
+        SD_Adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,array);
         listView.setAdapter(SD_Adapter);
 
-    }
-
-    public void onSaveData(View view){
-
         sp = getSharedPreferences("shared", MODE_PRIVATE);
-        editor = sp.edit();
-
-        ArrayAdapter<Integer> SD_Adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,SD_Index);
-
-        if(SavedData_Length >= 8){
-            Toast.makeText(getApplicationContext(), "더 이상 만들 수 없음", Toast.LENGTH_SHORT).show();
-        } else {
-            SavedData_Length ++;
-            Toast.makeText(getApplicationContext(), " " + SavedData_Length + " ", Toast.LENGTH_SHORT).show();
-
-            SD_Index.add(SavedData_Length);
-
-            listView.setAdapter(SD_Adapter);
-
-            editor.putInt("SavedData_Length", SavedData_Length);
-            editor.putString("schedule", "텍스트 표기" + SavedData_Length);
-            editor.putInt("time", SavedData_Length);
-            editor.commit(); //완료한다.
+        SavedData_Length = sp.getInt("count",0);
+        Toast.makeText(getApplicationContext(), "현재 : " + SD_Index.size() +"&" +SavedData_Length, Toast.LENGTH_SHORT).show();
 
 
-            SavedData_Length = sp.getInt("SavedData_Length", SavedData_Length);
-            schedule = sp.getString("schedule", "");
-            time = sp.getInt("time", 0);
+        btn_add = findViewById(R.id.btn_add);
+        delAll = findViewById(R.id.delAll);
 
-            SD_Index.add(SavedData_Length);
-            Schedule_Index.add(schedule);
-            Time_Index.add(time);
 
-            TextView tvData = (TextView) findViewById(R.id.tv_data);
 
-            tvData.setText(SavedData_Length + schedule + time);
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sp = getSharedPreferences("shared", MODE_PRIVATE);
+                editor = sp.edit();
+                SavedData_Length++;
+                if(SavedData_Length > 8){
+                    Toast.makeText(getApplicationContext(), "추가 불가능", Toast.LENGTH_SHORT).show();
+                } else {
+                    editor.putInt("count",SavedData_Length);
+                    SD_Index.add(SavedData_Length);
+                    editor.commit();
+                    listView = findViewById(R.id.TimerList);
+                    listView.setAdapter(SD_Adapter);
+                    SD_Adapter.notifyDataSetChanged();
+                }
+            }
+        });
 
+        delAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sp = getSharedPreferences("shared", MODE_PRIVATE);
+                editor = sp.edit();
+                SavedData_Length = 0;
+                SD_Index.clear();
+                editor.clear();
+                editor.commit();
+                SD_Adapter.notifyDataSetChanged();
+                Toast.makeText(getApplicationContext(), "삭제 후 : " + SavedData_Length, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+    public void setStringArrayList(Context context,String key, ArrayList<String> valueList){
+        sp = PreferenceManager.getDefaultSharedPreferences(context);
+
+        SharedPreferences.Editor editor = sp.edit();
+        JSONArray jsonArray = new JSONArray();
+
+        for(int i = 0; i < valueList.size(); i++){
+            jsonArray.put(valueList.get(i));
+        }
+
+        if(!valueList.isEmpty()){
+            editor.putString(key, jsonArray.toString());
+        }else{
+            editor.putString(key, null);
         }
     }
 
-
-    public void onDeleteData(View v) {
-        sp = getSharedPreferences("shared", MODE_PRIVATE);
-        editor = sp.edit();
-        SD_Adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,SD_Index);
-        SavedData_Length = 0;
-        int AdCount = SD_Adapter.getCount();
-        for(int i = 0 ; i < AdCount; i++){
-            SD_Adapter.remove(i);
+    public ArrayList<String> getStringArrayList(Context context,String key){
+        sp = PreferenceManager.getDefaultSharedPreferences(context);
+        String json = sp.getString(key, null);
+        ArrayList<String> valueList = new ArrayList<>();
+        if(json != null){
+            try{
+                JSONArray jArray = new JSONArray(json);
+                for(int i = 0; i < jArray.length(); i++){
+                    String data = jArray.optString(i);
+                    valueList.add(data);
+                }
+            }catch(JSONException e){
+                e.printStackTrace();
+            }
         }
-        listView.setAdapter(SD_Adapter);
-        editor.clear();
-        editor.commit();
-
-        SavedData_Length = sp.getInt("SavedData_Length",0);
-        schedule = sp.getString("schedule", "");
-        time = sp.getInt("time", 0);
-        TextView tvData = (TextView) findViewById(R.id.tv_data);
-        tvData.setText(SavedData_Length + schedule + time);
+        return valueList;
     }
 }
