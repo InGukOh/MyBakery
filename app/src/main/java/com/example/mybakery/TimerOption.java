15f204d439dc41e57a1c1f4ff9275f62b22bf897
Trident12kilo <dhdlsrnr1231@naver.com>
2022-04-11 19:33:45 +0900

    MyBakery / 알람기능 추가중 _ Adapter 적용 확인 /

--- app/src/main/java/com/example/mybakery/TimerOption.java
+++ app/src/main/java/com/example/mybakery/TimerOption.java
@@ -10,6 +10,7 @@ import android.view.Gravity;
 import android.view.View;
 
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -17,6 +18,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class TimerOption extends AppCompatActivity {
@@ -26,12 +28,17 @@ public class TimerOption extends AppCompatActivity {
     int time; // 일정의 시간
 
     LinearLayout add_Timer_Layout;
+    ListView listView;
     EditText make_Schedule;
     EditText make_Time;
     Button make_dellBtn;
 
     SharedPreferences sp;
     SharedPreferences.Editor editor;
+
+    ArrayList<Integer> SD_Index = new ArrayList<>(8);
+    ArrayList<String> Schedule_Index = new ArrayList<>(8);
+    ArrayList<Integer> Time_Index = new ArrayList<>(8);
 
     public int saveDATA(int count){
         count = sp.getInt("count",SavedData_Length);;
@@ -42,19 +49,29 @@ public class TimerOption extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.option_timer_setting);
 
+        ArrayAdapter<Integer> SD_Adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,SD_Index);
+
+        listView = findViewById(R.id.TimerList);
+
+        listView.setAdapter(SD_Adapter);
+
         sp = getSharedPreferences("shared", MODE_PRIVATE);
         editor = sp.edit();
 
         add_Timer_Layout = (LinearLayout) findViewById(R.id.add_Timer_Layout);
 
-        SavedData_Length = sp.getInt("count",0);
-
-        for(int i = 0; i <= SavedData_Length; i++){
-            int count = sp.getInt("count",0);
-            String schedule = sp.getString("schedule"+i, "");
-            int ScheduleCount = sp.getInt("time"+i,0);
-            TextView tvData = (TextView)findViewById(R.id.tv_data);
-            tvData.setText(SavedData_Length + schedule + ScheduleCount);
+        SavedData_Length = sp.getInt("SavedData_Length",0);
+        
+        if(SavedData_Length == 0){
+            Toast.makeText(getApplicationContext(), "타이머를 생성 해주세요", Toast.LENGTH_SHORT).show();
+        } else {
+            for(int i = 0; i <= SavedData_Length; i++) {
+                String schedule = sp.getString("schedule" + i, "텍스트 표기" + SavedData_Length);
+                int ScheduleCount = sp.getInt("time" + i, 0);
+                TextView tvData = (TextView) findViewById(R.id.tv_data);
+                tvData.setText(SavedData_Length + schedule + ScheduleCount);
+                listView.setAdapter(SD_Adapter);
+            }
         }
 
     }
@@ -64,9 +81,7 @@ public class TimerOption extends AppCompatActivity {
         sp = getSharedPreferences("shared", MODE_PRIVATE);
         editor = sp.edit();
 
-        make_Schedule = new EditText(getApplicationContext());
-        make_Time = new EditText(getApplicationContext());
-        make_dellBtn = new Button(getApplicationContext());
+        ArrayAdapter<Integer> SD_Adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,SD_Index);
 
         if(SavedData_Length >= 8){
             Toast.makeText(getApplicationContext(), "더 이상 만들 수 없음", Toast.LENGTH_SHORT).show();
@@ -74,56 +89,29 @@ public class TimerOption extends AppCompatActivity {
             SavedData_Length ++;
             Toast.makeText(getApplicationContext(), " " + SavedData_Length + " ", Toast.LENGTH_SHORT).show();
 
-            LinearLayout add_Schedule = new LinearLayout(getApplicationContext());
-            add_Schedule.setOrientation(LinearLayout.HORIZONTAL);
-            add_Schedule.setGravity(Gravity.CENTER);
+            SD_Index.add(SavedData_Length);
 
-            LinearLayout.LayoutParams setWnH =
-                    new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 210);
-            LinearLayout.LayoutParams Input_Schedule =
-                    new LinearLayout.LayoutParams(500, 200);
-            LinearLayout.LayoutParams InPut_Timer =
-                    new LinearLayout.LayoutParams(300, 200);
-            LinearLayout.LayoutParams Input_delBtn =
-                    new LinearLayout.LayoutParams(200, 200);
+            listView.setAdapter(SD_Adapter);
+
 
             for (int i = 0; i <= SavedData_Length; i++) {
 
-
-                add_Schedule.setLayoutParams(setWnH);
-                make_Schedule.setLayoutParams(Input_Schedule);
-                make_Time.setLayoutParams(InPut_Timer);
-                make_dellBtn.setLayoutParams(Input_delBtn);
-
-                make_Schedule.setHint("일정내용");
-                make_Time.setHint("시간");
-                make_dellBtn.setText("삭제");
-                make_dellBtn.setTextSize(15);
-
-                make_Schedule.setId(SavedData_Length);
-                make_Time.setId(SavedData_Length);
-                make_dellBtn.setId(SavedData_Length);
-                if(add_Schedule.getParent() != null){
-                    ((ViewGroup) add_Schedule.getParent()).removeView(add_Schedule);
-                    add_Schedule.addView(make_Schedule);
-                    add_Schedule.addView(make_Time);
-                    add_Schedule.addView(make_dellBtn);
-                }
-
-
-                add_Timer_Layout.addView(add_Schedule);
-
-                editor.putInt("count", SavedData_Length);
-                editor.putString("schedule" + i, "텍스트표기" + SavedData_Length);
+                editor.putInt("SavedData_Length", SavedData_Length);
+                editor.putString("schedule" + i, "텍스트 표기" + SavedData_Length);
                 editor.putInt("time" + i, SavedData_Length);
                 editor.commit(); //완료한다.
 
 
+                SavedData_Length = sp.getInt("SavedData_Length" + i, SavedData_Length);
                 schedule = sp.getString("schedule" + i, "");
                 time = sp.getInt("time" + i, 0);
 
+                SD_Index.add(SavedData_Length);
+                Schedule_Index.add(schedule);
+                Time_Index.add(time);
 
                 TextView tvData = (TextView) findViewById(R.id.tv_data);
+
                 tvData.setText(SavedData_Length + schedule + time);
             }
         }
@@ -138,9 +126,11 @@ public class TimerOption extends AppCompatActivity {
         SavedData_Length = 0;
         editor.clear();
         editor.commit();
-        String schedule = sp.getString("schedule", "");
-        int time = sp.getInt("time", 0);
+
+        SavedData_Length = sp.getInt("SavedData_Length",0);
+        schedule = sp.getString("schedule", "");
+        time = sp.getInt("time", 0);
         TextView tvData = (TextView) findViewById(R.id.tv_data);
-        tvData.setText(schedule + time);
+        tvData.setText(SavedData_Length + schedule + time);
     }
 }
